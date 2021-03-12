905aad8bab63bb54a3e692367ed7bb6596f3358c
zengrui <zengr666@163.com>
2021-03-12 23:48:02 +0800

    feat：daodao

---
+++ src/components/Money/EmptyData.vue
@@ -0,0 +1,28 @@
+<template>
+  <a-empty :image-style="{height: '60px'}">
+    <span slot="description" style="color: lightgrey">暂无数据</span>
+    <a-button type="primary" @click="newRecord">
+      记一笔
+    </a-button>
+  </a-empty>
+
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import {Component} from 'vue-property-decorator';
+
+@Component
+export default class EmptyData extends Vue  {
+    simpleImage: any;
+  beforeCreate() {
+    this.$store.commit('fetchRecords')
+  }
+  newRecord(){
+    this.$router.push('/money')
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
